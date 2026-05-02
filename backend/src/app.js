0c74cafc742705ff6251011aa17f53b1254a7033
Olufunke Johnston <ojohn003@gold.ac.uk>
2026-05-02 21:21:09 +0100

    Fix Express 5 wildcard route

--- backend/src/app.js
+++ backend/src/app.js
@@ -139,9 +139,10 @@ app.use(
       // Serve React frontend in production
       app.use(express.static(path.join(__dirname, "../../frontend/build")));
 
-      app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "../../frontend/build/index.html"));
-      });
+      app.use((req, res) => {
+      res.sendFile(path.join(__dirname, "../../frontend/build/index.html"));
+    });
+
 
 
   // Export app for use in server.js
