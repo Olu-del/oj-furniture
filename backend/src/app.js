d33c459e01ba94e8361f1143b253603273b7cfb7
Olufunke Johnston <ojohn003@gold.ac.uk>
2026-05-03 18:30:05 +0100

    Remove Railway origin and add Render frontend to CORS

--- backend/src/app.js
+++ backend/src/app.js
@@ -35,7 +35,7 @@ app.use(
 
 
 // Allow frontend to send cookies from either development port
-const allowedOrigins = ['http://localhost:3000', 'http://localhost:3002', 'https://oj-furniture-production.up.railway.app'];
+const allowedOrigins = ['http://localhost:3000', 'http://localhost:3002', 'https://oj-furniture-1.onrender.com'];
 app.use(cors({
   origin: function(origin, callback) {
     // allow requests with no origin 
