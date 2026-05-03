777472ceb8c34f916e68ed1545e4680db33f4b64
Olufunke Johnston <ojohn003@gold.ac.uk>
2026-05-03 08:26:37 +0100

    Fix frontend build path and update backend config

--- backend/src/app.js
+++ backend/src/app.js
@@ -137,12 +137,13 @@ app.use(
 
   
       // Serve React frontend in production
-      app.use(express.static(path.join(__dirname, "../../frontend/build")));
+      
 
-      app.use((req, res) => {
-      res.sendFile(path.join(__dirname, "../../frontend/build/index.html"));
+    app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+    app.get("*", (req, res) => {
+      res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
     });
-
 
 
   // Export app for use in server.js
