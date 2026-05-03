ac3a9049e13dc6d5843f7ce545748d6037c104e4
Olufunke Johnston <ojohn003@gold.ac.uk>
2026-05-03 17:37:30 +0100

    update frontend for deployment

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,26 +1,16 @@
-// import axios client for API calls
 import axios from "axios";
 
-// Axios instance configured for backend API calls with cookies
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "https://oj-furniture-production.up.railway.app/api",
-  withCredentials: true
+  baseURL: import.meta.env.VITE_API_URL || "https://oj-furniture.onrender.com/api",
+  withCredentials: true,
 });
 
-
-
-// Attach token automatically
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
-
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
-
   return config;
 });
-
-// api.defaults.headers.common["Authorization"] = `Bearer ${token}`;
-
 
 export default api;
